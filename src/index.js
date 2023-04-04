94cc31b5e5554bcfe9ff5eab1f81883717671eb0
tylermoroz <moroztyler@gmail.com>
2023-04-04 08:30:56 -0600

    Delete original files and start from scratch.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import tabs from './tabs.js';
-import home from './home.js';
-import './styles.css';
-
-content.setAttribute('id', 'content');
-
-document.body.appendChild(tabs());
-document.body.appendChild(home());
+console.log("hello webpack");
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,9 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Break & Brew',
-            template: './src/template.html',
-            inject: 'body',
-        }),
-    ],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
